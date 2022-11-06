f42b3ffa92816f02e9c6641b56df0be08afe7315
Mobin Zamanzadeh <108784865+mobinzamanzadeh@users.noreply.github.com>
2022-11-06 12:31:49 +0330

    Update settings.py

--- Login/settings.py
+++ Login/settings.py
@@ -15,15 +15,15 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'djangologinlogin@gmail.com'
-EMAIL_HOST_PASSWORD = 'loginlogin'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_USE_TLS = True
+# EMAIL_USE_SSL = False
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = 'djangologinlogin@gmail.com'
+# EMAIL_HOST_PASSWORD = 'loginlogin'
 
-DEFAULT_FROM_EMAIL = 'djangologinlogin@gmail.com'
+# DEFAULT_FROM_EMAIL = 'djangologinlogin@gmail.com'
 
 
 # Quick-start development settings - unsuitable for production
